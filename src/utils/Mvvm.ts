d180cb4c87eea72733008c5b2828c8900bea8811
disturbwe <2448510326@qq.com>
2020-01-24 01:15:55 +0800

    对proxy做了一些兼容性

--- src/utils/Mvvm.ts
+++ src/utils/Mvvm.ts
@@ -10,15 +10,20 @@ export class Mvvm {
         this.options = options;
         let observer = new Observer();
         observer.createObserve(options.data, this);//创建绑定的监听
+        let data;
+        if (!isIE()) {
+            data = this._data;
+        } else {
+            data = this._data = options.data
+        }
 
-        let data = this._data;
         let me = this;
-        
+
         Object.keys(data).forEach((key) => {//代理
             me._proxyData(key);
         });
 
-        
+
         this.compile = new Compile(options, this);//初始化视图
     }
 
@@ -28,10 +33,20 @@ export class Mvvm {
             configurable: false,
             enumerable: true,
             get() {
-                return Reflect.get(me._data, key)
+                if (!isIE()) {
+                    return Reflect.get(me._data, key)
+                } else {
+                    return me._data[key]
+                }
+
             },
             set(newVal) {
-                Reflect.set(me._data, key, newVal)
+                if (!isIE()) {
+                    Reflect.set(me._data, key, newVal)
+                } else {
+                    me._data[key] = newVal
+                }
+
             }
         });
     }
@@ -41,4 +56,12 @@ export interface OptionsAble {//配置可选项的接口
     el: string | object,
     data?: any,
     template?: string
+}
+
+export function isIE() {
+    if ("ActiveXObject" in window) {
+        return true;
+    } else {
+        return false;
+    }
 }
--- src/utils/Observer.ts
+++ src/utils/Observer.ts
@@ -1,5 +1,5 @@
 import Dep from './Dep';
-import { Mvvm } from './Mvvm';
+import { Mvvm, isIE } from './Mvvm';
 import { target } from './Watcher'
 /*实现Oberser类：当某个对象属性值变动的时候能通知订阅者*/
 export class Observer {
@@ -9,12 +9,18 @@ export class Observer {
             return;
         }
         this.$vm = vm;
-        this.$vm._data = this.defineReactive(ob)
-        // Object.keys(ob).forEach((key) => {
-        //     this.defineReactive(ob, key, ob[key]);
-        // })
+        let p;
+        if (!isIE()) {
+            this.$vm._data = this.defineReactive(ob)
+        } else {
+            console.log('ie')
+            Object.keys(ob).forEach((key) => {
+                this.defineReactive2(ob, key, ob[key]);
+            })
+        }
+
     }
-    defineReactive(ob: object, key?: string, val?: any) {
+    defineReactive(ob: object) {
         let dep = new Dep();
         let that = this;
         // let child: void = that.createObserve(val);
@@ -25,33 +31,34 @@ export class Observer {
             },
             set: function (targetob, key, newVal, receiver) {
                 //console.log(targetob, key, newVal, receiver);
-                if (newVal === val) {
-                    return;
-                }
                 //console.log('监听到变化了', `${val} -> ${newval}`)
-                val = newVal
-                // child = that.createObserve(val)
                 Reflect.set(targetob, key, newVal, receiver)
                 dep.notify()//变化时通知
                 return true;
             }
         });
-        // Object.defineProperty(ob, key, {
-        //     enumerable: true,//可枚举
-        //     configurable: false,//不能再define
-        //     get(): any /**在订阅者首次订阅的时候将其添加至订阅者列表 */ {
-        //         target && dep.depend()
-        //         return val
-        //     },
-        //     set(newval: string) {
-        //         if (newval === val) {
-        //             return;
-        //         }
-        //         //console.log('监听到变化了', `${val} -> ${newval}`)
-        //         val = newval
-        //         child = that.createObserve(val)
-        //         dep.notify()//变化时通知
-        //     }
-        // })
+
+    }
+    defineReactive2(ob: object, key?: string, val?: any) {
+        let dep = new Dep();
+        let that = this;
+        let child: void = that.createObserve(val, this.$vm);
+        Object.defineProperty(ob, key, {
+            enumerable: true,//可枚举
+            configurable: false,//不能再define
+            get(): any /**在订阅者首次订阅的时候将其添加至订阅者列表 */ {
+                target && dep.depend()
+                return val
+            },
+            set(newval: string) {
+                if (newval === val) {
+                    return;
+                }
+                //console.log('监听到变化了', `${val} -> ${newval}`)
+                val = newval
+                child = that.createObserve(val, this.$vm);
+                dep.notify()//变化时通知
+            }
+        })
     }
 }
